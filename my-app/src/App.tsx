6c06186a6bc1ce88dec670efb73d7e3d01b8f7ae
SakshiCode731 <sakshigautam90111@gmail.com>
2026-04-06 23:53:01 +0530

    there upadating old code

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,6 +3,7 @@ function App() {
     <div>
       <h1>Hello Sakshi Whats up! </h1>
       <p>My first Vite + React app</p>
+      <p>try to editing to some file to practice with rect js</p>
     </div>
   )
 }
