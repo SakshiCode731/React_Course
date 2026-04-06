cbe52316b2386ee705b90ed387c36a16ce8c935b
SakshiCode731 <sakshigautam90111@gmail.com>
2026-04-06 23:53:59 +0530

    little bit upadate it

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,7 +3,7 @@ function App() {
     <div>
       <h1>Hello Sakshi Whats up! </h1>
       <p>My first Vite + React app</p>
-      <p>try to editing to some file to practice with rect js</p>
+      {/* <p>try to editing to some file to practice with rect</p> */}
     </div>
   )
 }
