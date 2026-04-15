c44d12b00303fd23b382d47444fb310e08ecacc3
SakshiCode731 <sakshigautam90111@gmail.com>
2026-04-15 22:25:03 +0530

    adding code here

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,7 +3,7 @@ function App() {
     <div>
       <h1>Hello this is me begginer in development</h1>
       <p>My first Vite + React app</p>
-      <p>this is my first react app</p>
+      <p>Starting of web development</p>
       {/* <p>try to editing to some file to practice with rect</p> */}
     </div>
   )
