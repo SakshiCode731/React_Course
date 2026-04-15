377394771dfc19eb2b195976e1e0ee5bfc5cc9f6
SakshiCode731 <sakshigautam90111@gmail.com>
2026-04-15 22:23:39 +0530

    updating my code

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <h1>Hello Sakshi Whats up! </h1>
+      <h1>Hello this is me begginer in development</h1>
       <p>My first Vite + React app</p>
       <p>this is my first react app</p>
       {/* <p>try to editing to some file to practice with rect</p> */}
