33a6568584cb7a642b94aeaa39aa303f78984794
SakshiCode731 <sakshigautam90111@gmail.com>
2026-04-22 21:36:45 +0530

    updating the code

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -4,6 +4,7 @@ function App() {
       <h1>Hello this is me begginer in development</h1>
       <p>My first Vite + React app</p>
       <p>Starting of web development</p>
+      <p>learning react vite</p>
       <p>try to editing to some file to practice with rect</p>
     </div>
   )
