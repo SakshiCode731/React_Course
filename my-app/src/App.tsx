02519df0024b245d1c98012a39aa3ff81012bf5e
SakshiCode731 <sakshigautam90111@gmail.com>
2026-04-22 21:35:34 +0530

    working with file

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
       <h1>Hello this is me begginer in development</h1>
       <p>My first Vite + React app</p>
       <p>Starting of web development</p>
-      {/* <p>try to editing to some file to practice with rect</p> */}
+      <p>try to editing to some file to practice with rect</p>
     </div>
   )
 }
