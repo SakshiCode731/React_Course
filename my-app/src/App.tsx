cf4b9e693e22b3589b27a7ab66e650f481aa213a
SakshiCode731 <sakshigautam90111@gmail.com>
2026-04-23 22:42:21 +0530

    writing new code here

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,6 +2,7 @@ function App() {
   return (
     <div>
       <h1>Hello this is me begginer in development</h1>
+      <h2>excited to begging about React and Vite</h2>
       <p>My first Vite + React app</p>
       <p>Starting of web development</p>
       <p>learning react vite</p>
