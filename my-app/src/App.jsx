eff61b37e93cc9efc1d7ad93866413d662c01102
SakshiCode731 <sakshigautam90111@gmail.com>
2026-05-21 20:54:24 +0530

    updating the code

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -132,7 +132,7 @@ function App(){
         {/* <!-- Mobile menu button--> */}
         <button type="button" command="--toggle" commandfor="mobile-menu" className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-white/5 hover:text-white focus:outline-2 focus:-outline-offset-1 focus:outline-indigo-500">
           <span className="absolute -inset-0.5"></span>
-          <span className="sr-only">Open main menu</span>
+          <span className="sr-only">Open main menus</span>
           <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon" aria-hidden="true" className="size-6 in-aria-expanded:hidden">
             <path d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" stroke-linecap="round" stroke-linejoin="round" />
           </svg>
