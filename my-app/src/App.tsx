9aafb1c52b54add3ef884cba814a3dbe1077be5f
SakshiCode731 <sakshigautam90111@gmail.com>
2026-05-22 12:10:49 +0530

    working with propstype

---
+++ my-app/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello this is me begginer in development</h1>
+      <p>My first Vite + React app</p>
+      <p>Starting of web development</p>
+      {/* <p>try to editing to some file to practice with rect</p> */}
+    </div>
+  )
+}
+
+export default App
