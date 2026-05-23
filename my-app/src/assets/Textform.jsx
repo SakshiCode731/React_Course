8bdf88131cc09fda670339a0864694a9bb2645eb
SakshiCode731 <sakshigautam90111@gmail.com>
2026-05-23 21:19:28 +0530

    writing code for state

--- my-app/src/assets/Textform.jsx
+++ my-app/src/assets/Textform.jsx
@@ -1,12 +1,26 @@
+import React , { useState } from 'react'
+
 export default function Textform(props) {
+    const handleUpClick = () => {
+        console.log("Uppercase was clicked");
+        setText("You have clicked on handleUpClick")
+    }
+
+    const handleOnChange = ()=>{
+        console.log("On change");
+    }
+    const [text, setText] = useState("Enter text here");
+    // setText("new text");   to chnage the text use like this
+    // text= newtext;   wrong way to chnage the state
     return (
-    <div>
-        <h1>{props.heading}</h1>
-        <div className="mb-3">
-            <label for ="myBox" className="form-label">Example textarea</label>
-            <textarea className="form-control" id="myBox" rows="8"></textarea>
-            <h1>Textform</h1>
-        </div>
+        <div>
+            <h1>{props.heading}</h1>
+            <div className="mb-3">
+                <textarea className="w-full border border-gray-400 rounded p-2" id="myBox" value={text} onChange={handleOnChange} rows="8"></textarea>
+                <button className="bg-blue-500 text-white px-4 py-2 rounded" onClick={handleUpClick}>
+                    Convert to Uppercase
+                </button>
+            </div>
         </div>
     )
 }
