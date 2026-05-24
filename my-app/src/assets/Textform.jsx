3c92c3978523108aec82a8f17251932cd2cba9f6
SakshiCode731 <sakshigautam90111@gmail.com>
2026-05-24 21:37:13 +0530

    adding code of hooks

--- my-app/src/assets/Textform.jsx
+++ my-app/src/assets/Textform.jsx
@@ -15,14 +15,20 @@ export default function Textform(props) {
     // setText("new text");   to chnage the text use like this
     // text= newtext;   wrong way to chnage the state
     return (
-        <div>
+        <>
+        <div className="container mx-auto">
             <h1>{props.heading}</h1>
             <div className="mb-3">
                 <textarea className="w-full border border-gray-400 rounded p-2" id="myBox" value={text} onChange={handleOnChange} rows="8"></textarea>
+                </div>
                 <button className="bg-blue-500 text-white px-4 py-2 rounded" onClick={handleUpClick}>
                     Convert to Uppercase
                 </button>
             </div>
+            <div className="container mx-auto my-3">
+                <h2>Your text summary  </h2>
+                <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and  {text.length} character</p>
         </div>
+        </>
     )
 }
