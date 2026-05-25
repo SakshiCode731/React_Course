c25acf132e013a998d52b957e06d55d777de5bba
SakshiCode731 <sakshigautam90111@gmail.com>
2026-05-25 12:16:48 +0530

    there is adding some code to textutils

--- my-app/src/assets/Textform.jsx
+++ my-app/src/assets/Textform.jsx
@@ -2,11 +2,16 @@ import React , { useState } from 'react'
 
 export default function Textform(props) {
     const handleUpClick = () => {
-        console.log("Uppercase was clicked");
+        // console.log("Uppercase was clicked");
         let newText = text.toUpperCase();
         setText(newText);
     }
 
+     const handleLoClick = () => {
+        // console.log("Lowercase was clicked");
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
     const handleOnChange = (event)=>{
         console.log("On change");
         setText(event.target.value);
@@ -21,13 +26,19 @@ export default function Textform(props) {
             <div className="mb-3">
                 <textarea className="w-full border border-gray-400 rounded p-2" id="myBox" value={text} onChange={handleOnChange} rows="8"></textarea>
                 </div>
-                <button className="bg-blue-500 text-white px-4 py-2 rounded" onClick={handleUpClick}>
+                <button className="bg-blue-500 text-white px-4 py-2 rounded mx-2" onClick={handleUpClick}>
                     Convert to Uppercase
+                </button>
+                <button className="bg-blue-500 text-white px-4 py-2 rounded" onClick={handleLoClick}>
+                    Convert to Lowercase
                 </button>
             </div>
             <div className="container mx-auto my-3">
                 <h2>Your text summary  </h2>
                 <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and  {text.length} character</p>
+                <p>{0.008 * text.split(" ").length}Minute read</p>
+                <h2>Preview</h2>
+                <p>{text}</p>
         </div>
         </>
     )
