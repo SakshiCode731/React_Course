d476e36932e24fc0c32895913587123249e82883
SakshiCode731 <sakshigautam90111@gmail.com>
2026-06-09 22:47:12 +0530

    there is adding some code

--- my-app/src/assets/Navbar.jsx
+++ my-app/src/assets/Navbar.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Navbar({ title = "TextUtils", aboutText = "About" }) {
+function Navbar( props ) {
+  const isDark = props.mode == 'dark';
+  const navBg = isDark ? 'bg-gray-800' : 'bg-white';
+  const navText = isDark ? 'text-white' : 'text-black';
   return (
     <nav className={`bg-${props.mode } text-${props.mode} px-4 py-3`}>
       <div className="container mx-auto flex items-center justify-between">
 
         {/* Logo */}
         <a href="#" className="text-2xl font-bold">
-          {props.mode}
+          {props.title}
         </a>
 
         {/* Navigation Links */}
@@ -34,7 +37,9 @@ function Navbar({ title = "TextUtils", aboutText = "About" }) {
   <input
     type="search"
     placeholder="Search"
-    className="rounded-md border border-gray-600 bg-gray-800 px-3 py-2 text-white outline-none focus:border-green-400"
+    className={`rounded-md border px-3 py-2 outline-none focus:border-green-400 ${
+              isDark ? 'border-gray-700 bg-gray-800 text-white' : 'border-gray-300 bg-white text-black'
+            }`}
   />
 
   <button
